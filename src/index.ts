2256771acf771192fa123f8b9fb3a936c109bb98
Duck <118633132+Dxckrr@users.noreply.github.com>
2024-08-30 12:32:30 -0500

    feat database connection

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import morgan from 'morgan';
 import path from 'path';
 import * as dotenv from 'dotenv';
 dotenv.config({ path: path.join(__dirname, '../environment/.env') });
+import mysql from 'mysql2/promise';
+import connection from './providers/database'; // Ajusta la ruta según tu estructura de carpetas
+
 
 const app = express();
 const port = 3000;
@@ -27,6 +30,17 @@ app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server is listening on ${port}`);
+  const testConnection = async (): Promise<void> => {
+    try {
+      const [rows]: any[] = await connection.query('SELECT 1 + 1 AS result');
+      console.log('Connection successful:', rows[0].result);
+      console.log('Connection successful:', rows[0].result === 2);
+    } catch (error: any) {
+      console.error('Connection failed:', error.message);
+    }
+  };
+  testConnection();
+
 });
 
 app.on('error', (err: any) => {
